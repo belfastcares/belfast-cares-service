9d0cd3103cfd794f497daa83ccb2074786cd0faa
Kyle Harrison <kyle.harrison@alertlogic.com>
2016-09-10 12:56:58 +0100

    Adding libs and static assets to package

--- base_site/urls.py
+++ base_site/urls.py
@@ -5,12 +5,7 @@ admin.autodiscover()
 
 import web_app.views
 
-# Examples:
-# url(r'^$', 'base_site.views.home', name='home'),
-# url(r'^blog/', include('blog.urls')),
-
 urlpatterns = [
     url(r'^$', web_app.views.index, name='index'),
-    url(r'^db', web_app.views.db, name='db'),
     url(r'^admin/', include(admin.site.urls)),
 ]
