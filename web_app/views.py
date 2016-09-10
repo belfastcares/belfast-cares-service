ff9bf65db1631ae854c01bbd2183e0d79f1c8e35
Adam <fatal.toxicity@gmail.com>
2016-09-10 16:07:28 +0100

    Initial work on showing charity details

--- web_app/views.py
+++ web_app/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import *
 
 
 def index(request):
@@ -21,8 +22,15 @@ def charities_listing(request):
     return render(request, 'charities_single.html')
 
 
-def charities_single(request):
-    return render(request, 'charities_single.html')
+def charities_single(request, id):
+
+    organisation = Organisation.objects.filter(id=id)[0]
+    org_address = organisation.address
+    org_wishlist =organisation.wishlist
+
+    return render(request, 'charities_single.html', {'organisation': organisation,
+                                                     'org_address': org_address,
+                                                     'org_wishlist': org_wishlist})
 
 
 def help(request):
