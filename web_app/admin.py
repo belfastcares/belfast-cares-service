5770c818ee854c1adb2c9b4974fb112f5c383139
Adam <fatal.toxicity@gmail.com>
2016-09-26 21:12:28 +0100

    Improvements to Organisation admin page, incoporated django user model
    - Several organisation details now editable inline, for example the wishlist.
    - Implement better multiselect for wishlist item selection
    - Added model method to organisation to calculate percentage towards goal
    - Slight tweaks to organisation list and single pages to agree with model changes
    
    *** Requirements change ***
    Previous commit included use of ImageField for uploading organisation logos. This requires pillow image library for features such as verifying file is an image.

--- web_app/admin.py
+++ web_app/admin.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 
 # Register your models here.
 from django import forms
-
 from .models import *
+
+# Customise default admin
+admin.AdminSite.site_header = "Belfast Cares Administration"
+admin.AdminSite.site_title = "Belfast Cares Site Admin"
+
 
 class WishlistForm(forms.ModelForm):
     class Meta:
@@ -11,10 +15,25 @@ class WishlistForm(forms.ModelForm):
         fields = '__all__'
 
     def clean(self):
+        super(WishlistForm, self).clean()
         start_date = self.cleaned_data.get('start_time')
         end_date = self.cleaned_data.get('end_time')
         if start_date > end_date:
             raise forms.ValidationError("End time cannot come before start time.")
+        return self.cleaned_data
+
+
+class OrganisationForm(forms.ModelForm):
+    class Meta:
+        model = Organisation
+        fields = '__all__'
+
+    def clean(self):
+        super(OrganisationForm, self).clean()
+        raised = self.cleaned_data.get('raised')
+        goal = self.cleaned_data.get('goal')
+        if raised < 0 or goal < 0:
+            raise forms.ValidationError("Raised or goal cannot be less than 0.")
         return self.cleaned_data
 
 
@@ -23,16 +42,40 @@ class WishlistAdmin(admin.ModelAdmin):
     filter_horizontal = ('items',)
 
 
+class OrganisationUserInline(admin.StackedInline):
+    model = OrganisationUser
+    extra = 1
+
+
+class WishlistInline(admin.StackedInline):
+    form = WishlistForm
+    model = Wishlist
+    filter_horizontal = ('items',)
+    verbose_name_plural = 'Wishlist'
+
+
 class OrganisationAdmin(admin.ModelAdmin):
-    fields = ('name', ('image', 'image_preview_large'), 'primary_contact', 'address', 'description', 'wishlist',
-              'just_giving_link', ('raised', 'goal'))
+    form = OrganisationForm
+    fieldsets = (
+        ('Basic Details', {
+            'fields': ('name', ('image', 'image_preview_large'), 'description', 'primary_contact', 'address')
+        }),
+        ('Fund Raising Details', {
+            'fields': ('just_giving_link', ('raised', 'goal'))
+        })
+    )
+
+    list_display = ('image_preview_small', 'name', 'primary_contact', 'wishlist', 'address', 'description',
+                    'just_giving_link', 'raised', 'goal', 'associated_user_accounts')
+    list_display_links = ('image_preview_small', 'name')
+    inlines = [OrganisationUserInline, WishlistInline]
     readonly_fields = ('image_preview_large',)
-    list_display = ['name', 'image_preview_small', 'primary_contact', 'address', 'description', 'wishlist', 'just_giving_link', 'raised', 'goal']
+    search_fields = ['name']
 
 
-admin.site.register(User)
 admin.site.register(Organisation, OrganisationAdmin)
 admin.site.register(Contact)
 admin.site.register(Address)
 admin.site.register(Wishlist, WishlistAdmin)
 admin.site.register(Item)
+admin.site.register(OrganisationUser)
--- web_app/models.py
+++ web_app/models.py
@@ -3,10 +3,13 @@ from __future__ import unicode_literals
 import os
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
+from django.contrib.auth.models import User
+from django.utils.html import format_html
+
 
 @python_2_unicode_compatible
 class Address(models.Model):
-    address_line = models.CharField('address_line', max_length=100)
+    address_line = models.CharField('address line', max_length=100)
     county = models.CharField('country', max_length=50)
     postcode = models.CharField('postcode', max_length=10)
 
@@ -16,7 +19,7 @@ class Address(models.Model):
 
 @python_2_unicode_compatible
 class Contact(models.Model):
-    first_name = models.CharField('first_name', max_length=30)
+    first_name = models.CharField('first name', max_length=30)
     surname = models.CharField('surname', max_length=30)
     telephone = models.CharField('telephone', max_length=15)
     mobile = models.CharField('mobile', max_length=15)
@@ -35,16 +38,6 @@ class Item(models.Model):
     def __str__(self):
         return self.name
 
-@python_2_unicode_compatible
-class Wishlist(models.Model):
-    start_time = models.DateTimeField('start_time')
-    end_time = models.DateTimeField('end_time')
-    reoccurring = models.BooleanField('reoccurring')
-    items = models.ManyToManyField(Item)
-
-    def __str__(self):
-        return "wishlist " + str(self.id)
-
 def get_logo_file_name(instance, filename):
     return os.path.join('uploads', instance.name, 'logo' + os.path.splitext(filename)[1])
 
@@ -55,7 +48,6 @@ class Organisation(models.Model):
     primary_contact = models.OneToOneField(Contact, on_delete=models.CASCADE)
     address = models.OneToOneField(Address, on_delete=models.CASCADE)
     description = models.TextField('description')
-    wishlist = models.OneToOneField(Wishlist, on_delete=models.CASCADE)
     just_giving_link = models.URLField('just giving link', max_length=255, blank=True)
     raised = models.DecimalField('raised', max_digits=25, decimal_places=2, blank=True)
     goal = models.DecimalField('goal', max_digits=25, decimal_places=2, blank=True)
@@ -65,26 +57,50 @@ class Organisation(models.Model):
 
     def image_preview_large(self):
         if self.image:
-            return '<img src="%s" width="150" height="150"/>' % self.image.url
+            return format_html(
+                '<img src="{}" width="150" height="150"/>',
+                self.image.url
+            )
         else:
             return 'No Logo'
-    image_preview_large.allow_tags = True
-    image_preview_large.short_description = 'Logo Preview'
+    image_preview_large.short_description = 'Image Preview'
 
     def image_preview_small(self):
         if self.image:
-            return '<img src="%s" width="50" height="50"/>' % self.image.url
+            return format_html(
+                '<img src="{}" width="50" height="50"/>',
+                self.image.url
+            )
         else:
             return 'No Logo'
-    image_preview_small.allow_tags = True
     image_preview_small.short_description = 'Image Preview'
 
+    def associated_user_accounts(self):
+        return ','.join(str(item.user.id) +': ' + item.user.username for item in self.organisationuser_set.all())
+    associated_user_accounts.short_description = 'User Accounts'
+
+    def percentage_to_fund_raising_goal(self):
+        val = int(round(self.raised / self.goal * 100))
+        if val > 100:
+            return 100
+        return val
+
 @python_2_unicode_compatible
-class User(models.Model):
-    username = models.CharField('username', unique=True, max_length=80)
-    password = models.CharField('password', max_length=100)
-    contact = models.OneToOneField(Contact, on_delete=models.CASCADE)
+class Wishlist(models.Model):
+    organisation = models.OneToOneField(Organisation, on_delete=models.CASCADE)
+    start_time = models.DateTimeField('start_time')
+    end_time = models.DateTimeField('end_time')
+    reoccurring = models.BooleanField('reoccurring')
+    items = models.ManyToManyField(Item)
+
+    def __str__(self):
+        return self.organisation.name + " Wishlist"
+
+@python_2_unicode_compatible
+class OrganisationUser(models.Model):
+    user = models.OneToOneField(User, verbose_name="User account details")
+    contact = models.OneToOneField(Contact, on_delete=models.CASCADE, verbose_name='Contact details')
     organisation = models.ForeignKey(Organisation, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.username
+        return self.organisation.name + " Organisation User " + str(self.user.username)
