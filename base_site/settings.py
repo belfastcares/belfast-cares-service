d2d01170530fe545087e0e709b7a4ba90240e7bb
Kyle Harrison <kyle.harrison@alertlogic.com>
2016-10-08 16:48:19 +0100

    Modifying settings to allow site users to access API once authenticated

--- base_site/settings.py
+++ base_site/settings.py
@@ -80,7 +80,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAdminUser',
+        'rest_framework.permissions.IsAuthenticated',
     ),
 }
 
